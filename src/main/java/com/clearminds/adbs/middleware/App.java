5ec0ff337785a6e33d5e23ffb92ac22e1ea99e31
AndresBetancourt-Dev <contacto.betancourth@gmail.com>
2020-12-09 09:37:00 -0500

    🕋 Versión 1.0 Initial

---
+++ src/main/java/com/clearminds/adbs/middleware/App.java
@@ -0,0 +1,13 @@
+package com.clearminds.adbs.middleware;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
